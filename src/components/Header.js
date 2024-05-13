9a9d94f64806465845f09c320d32dded25afb40e
Adolfo Nava <adolfonava9960@gmail.com>
2024-05-13 09:23:18 -0500

    Folder restructuring

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import { Navbar, NavbarBrand } from "reactstrap";
+import { Container,Navbar, NavbarBrand } from "reactstrap";
 import NucampLogo from "../app/assets/img/logo.png";
 
 const Header = () => {
     return (
         <Navbar dark color="primary" sticky='top' expand='md'>
-
+            <Container>
                 <NavbarBrand href='/'>
                     <img src={NucampLogo} alt='nucamp logo' />
                 </NavbarBrand>
-
+                </Container>
         </Navbar>
     );
 };
