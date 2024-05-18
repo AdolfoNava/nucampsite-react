be6686e39ea541988b24fb3380863b6779b27ce3
Adolfo Nava <adolfonava9960@gmail.com>
2024-05-17 22:49:16 -0500

    Simple Routes implementation

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@
 import './App.css';
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
+import {Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
+import ContactPage from './pages/ContactPage.js';
 //import CampsitesList from "./features/campsites/CampsitesList.js";
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage.js';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <CampsitesDirectoryPage/>
-      <Footer/>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='contact' element={<ContactPage/>}/>
+        <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+      </Routes>
+      <Footer />
     </div>
   );
 }
