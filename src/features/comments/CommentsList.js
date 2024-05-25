e929052d43740aa520fe571b0d72d51b3c8c59d9
Adolfo Nava <adolfonava9960@gmail.com>
2024-05-25 12:02:58 -0500

    Week 3 work done

--- src/features/comments/CommentsList.js
+++ src/features/comments/CommentsList.js
@@ -1,7 +1,7 @@
 import {Col} from 'reactstrap';
 import Comment from './Comment';
 import { selectCommentsByCampsiteId } from './commentsSlice';
-
+import CommentForm from './CommentForm';
 const CommentsList = ({campsiteId}) => {
     const comments = selectCommentsByCampsiteId(campsiteId);
     if(comments && comments.length > 0){
@@ -11,13 +11,16 @@ const CommentsList = ({campsiteId}) => {
                 {comments.map((comment) => {
                     return <Comment key={comment.id} comment={comment}/>;
                 })}
+                <CommentForm campsiteId={campsiteId}/>
             </Col>
         );
     }
     return (
         <Col md='5' className='m-1'>
             There are no comments for this campsite yet.
-        </Col>
+            <CommentForm/>
+        </Col>    
     );
+
 }
 export default CommentsList;
